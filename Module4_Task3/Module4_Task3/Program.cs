631a0121cadec8e2a7b4e0cc75621bafb488f525
Vira Vostrikova <vvostrikova@csod.com>
2021-04-20 12:17:15 +0300

    Initial commit

---
+++ Module4_Task3/Module4_Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Module4_Task3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var starter = new Starter();
+            starter.Run();
+        }
+    }
+}
